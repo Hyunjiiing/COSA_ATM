1a68a9b2eef5af266a4404b060160e31efac636e
KangJiUng <woong236406@gmail.com>
2023-08-12 16:08:15 +0900

    Add bottom bar in settings page

--- Frontend/lib/settings.dart
+++ Frontend/lib/settings.dart
@@ -14,7 +14,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+int currentTap=0;
+
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +45,7 @@ class SettingsPage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '사용자 이름 또는 캐릭터 이름',
+                        'LeeDY',
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 17,
@@ -243,6 +250,223 @@ class SettingsPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 16.0),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 60,
+          child: Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      MaterialButton(
+                        padding: EdgeInsets.zero,
+                        minWidth: 40,
+                        onPressed: (){
+                          setState((){
+                            currentTap=0;
+                            /*Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=>map_page())
+                              );*/
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 10),
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: currentTap==0 ?  Color(0xFFFFCD4A) : Colors.transparent,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    width:30,
+                                    height: 30,
+                                    child: Image.asset(
+                                      "images/map.png",
+                                      color: currentTap==0 ? Colors.black : Colors.grey,
+                                    ),
+                                  ),
+                                  Text(
+                                    '지도',
+                                    style: TextStyle(color: currentTap==0 ? Colors.black : Colors.grey),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ), //홈
+                      SizedBox(width: 38),
+                      MaterialButton(
+                        minWidth: 40,
+                        padding: EdgeInsets.zero,
+                        onPressed: (){
+                          setState((){
+                            currentTap=1;
+                            /*Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=> map_page())
+                              );*/
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 10),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: currentTap==1?  Color(0xFFFFCD4A) : Colors.transparent,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 30,
+                                height: 30,
+                                child: Image.asset(
+                                  "images/petprint.png",
+                                  color: currentTap==1 ? Colors.black : Colors.grey,
+
+                                ),
+                              ),
+                              Text(
+                                '펫',
+                                style: TextStyle(color: currentTap==1 ? Colors.black : Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 38),
+                      MaterialButton(
+                        minWidth: 40,
+                        padding: EdgeInsets.zero,
+                        onPressed: (){
+                          setState((){
+                            currentTap=2;
+                            /* Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=>shop_page())
+                              );*/
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: currentTap==2?  Color(0xFFFFCD4A) : Colors.transparent,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.camera_alt,
+                                color: currentTap==2 ? Colors.black : Colors.grey,
+                              ),
+                              Text(
+                                '카메라',
+                                style: TextStyle(color: currentTap==2 ? Colors.black : Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ), //유저
+                      SizedBox(width: 38),
+                      MaterialButton(
+                        minWidth: 40,
+                        padding: EdgeInsets.zero,
+                        onPressed: (){
+                          setState((){
+                            currentTap=3;
+                            /* Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=>shop_page())
+                              );*/
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: currentTap==3?  Color(0xFFFFCD4A) : Colors.transparent,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.shopping_bag_outlined,
+                                color: currentTap==3 ? Colors.black : Colors.grey,
+                              ),
+                              Text(
+                                '상점',
+                                style: TextStyle(color: currentTap==3 ? Colors.black : Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ), //상점
+                      SizedBox(width: 38),
+                      MaterialButton(
+                        minWidth: 40,
+                        padding: EdgeInsets.zero,
+                        onPressed: (){
+                          setState((){
+                            currentTap=4;
+                            /*Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=>map_page())
+                              );*/
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 10),
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: currentTap==4?  Color(0xFFFFCD4A) : Colors.transparent,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.settings,
+                                    color: currentTap==4 ? Colors.black : Colors.grey,
+                                  ),
+                                  Text(
+                                    '설정',
+                                    style: TextStyle(color: currentTap==4 ? Colors.black : Colors.grey),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ],
           ),
         ),
